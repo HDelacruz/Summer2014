ae84964a5328a9a06d7954a1efdbf3076f2db649
001320017 <001320017@N205A4.labs.neit.edu>
2014-07-29 19:47:49 -0400

    week2

--- public_html/week2/demo/script.js
+++ public_html/week2/demo/script.js
@@ -1,7 +1,31 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+    var email = document.getElementById('email');
+    var err_email = document.getElementById('err_email');
+    var hasErrors = false;
+    
+    function submit() {
+        
+        if ( !email.value.length ) {
+           hasErrors = true;
+           err_email.innerHTML = '<p>Email is not valid.</p>';
+           //https://developer.mozilla.org/en-US/docs/Web/API/Element.classList
+           email.classList.add('bad');
+           email.classList.remove('good');
+            
+        } else {
+            err_email.innerHTML = '';
+            email.classList.add('good');
+            email.classList.remove('bad');
+        }
+        
+        
+    }
+    
+    
+    
+    // method
+    function testing( str ){
+        console.log(str);
+   }
 
+//testing('helloooooooooooo');
