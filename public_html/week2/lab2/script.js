17a45abef4574e47c15d4956acce002840976a1f
001320017 <001320017@N205A4.labs.neit.edu>
2014-07-29 20:13:17 -0400

    week2

--- public_html/week2/lab2/script.js
+++ public_html/week2/lab2/script.js
@@ -24,6 +24,29 @@
         fullname.classList.add('bad');       
         fullnameErr.innerHTML = "<p>Full Name is not valid.</p>";       
     }
+   var email = document.getElementById('email');
+    var err_email = document.getElementById('err_email');
+    
+        
+        if ( !email.value.length ) {
+           hasErrors = true;
+           err_email.innerHTML = '<p>Email is not valid.</p>';
+           email.classList.add('bad');
+           email.classList.remove('good');
+            
+        } else {
+            err_email.innerHTML = '';
+            email.classList.add('good');
+            email.classList.remove('bad');
+        }
+        
+        
+    }
+    
+   
+    function testing( str ){
+        console.log(str);
+    
     
     
     
