a8491e257caf55680d633d6d02c3cd18f74aa0aa
001320017 <001320017@N205A4.labs.neit.edu>
2014-09-16 22:35:08 -0400

    week2

--- public_html/Final/script.js
+++ public_html/Final/script.js
@@ -7,7 +7,7 @@
   
  // this is used to check the email field 
 function checkEmail ( str ) {
-      var myreg2 = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+      var myreg2 = /^[a-zA-Z]+[@]{1}[a-zA-Z]+[\.]{1}[a-zA-Z]{2,3}$/;
       return myreg2.test(str);
 }
 
@@ -34,7 +34,7 @@ function submitForm() {
 
        // this are my new variables using get element by id  
         var fullname = document.getElementById('fullname');
-		var email = document.getElementById('email');
+	var email = document.getElementById('email');
         var phone = document.getElementById('phone');       
         var description = document.getElementById('description');
 
@@ -43,19 +43,23 @@ function submitForm() {
     if(fullname.value === "")
     {
         var msg="please enter Full name";
-        document.getElementById("fullname_err").innerHTML=msg; 
-		 console.log("please enter your Full name");
+        document.getElementById("fullname_err").innerHTML=msg; 		 
     }
-    else
-    {
-       if ( noSpaceAlphaValidate( fullname.value ) === true ) {
-                console.log("full name is good");
-        } else {
-                console.log("full name only alpha characters");
+    else if ( noSpaceAlphaValidate( fullname.value ) === false ) 
+        {
+            var msg="full name only alpha characters";
+            document.getElementById("fullname_err").innerHTML=msg;
+        } 
+        else 
+        {
+           hasErrors = false;
+           fullname.classList.remove('good');
         }
-         document.getElementById("fullname_err").innerHTML=""; 
-		 
-    }
+          
+           //email.classList.add('bad');
+          // email.classList.remove('good');
+       
+         
 	
 	//Checking email 
     
@@ -63,20 +67,19 @@ function submitForm() {
    if(email.value === "")
     {
         var msg="please enter email";
+        document.getElementById("email_err").innerHTML=msg; 	 
+    }
+    else if ( checkEmail( email.value ) === false) 
+        {
+                var msg="Email follow this format Test@test.com";
         document.getElementById("email_err").innerHTML=msg; 
-		 console.log("please enter email");
-    }
-    else
-    {
-       if ( checkEmail( email.value ) === true) {
-                console.log("Email not valid");
-        } else {
-                console.log("Email is good");
+        } 
+       	
+        else 
+        {
+           hasErrors = false;
+           email.classList.remove('good');
         }
-         document.getElementById("email_err").innerHTML=""; 
-		 
-    }
-		
    
    //Checking if phone is good 
     
@@ -84,100 +87,96 @@ function submitForm() {
     {
         var msg="please enter Phone Number";
         document.getElementById("phone_err").innerHTML=msg; 
-		 console.log("please enter your Phone");
-    }
-    else
-    {
-       if ( checkPhone( phone.value ) === true ) {
-                console.log("Phone Number is good");
-        } else {
-                console.log("phone not good");
-        }
-         document.getElementById("phone_err").innerHTML=""; 
 		 
     }
-            
+    else if ( checkPhone( phone.value ) === false ) 
+        {
+          var msg="please this is the format 000-000-0000";
+          document.getElementById("phone_err").innerHTML=msg; 
+        } 
+       else 
+        {
+           hasErrors = false;
+           phone.classList.remove('good');
+        }
     
   //remove html elements
     
-    description.value = strip_HTML(description.value);
     
     if (description.value === "" || description.value.length > 150)
     {
-        var msg="please enter valid Description";
-        document.getElementById("description_err").innerHTML=msg;
-		console.log("Please Enter a Description");
+        var msg="please enter a valid description less then 150 characters";
+        document.getElementById("description_err").innerHTML=msg;	
     }
     
-    else 
-    {
-	console.log("Description is good")
- 
- // error message if comments are valid
- 
-        document.getElementById("description_err").innerHTML="";
-        
+    else if (strip_HTML (description.value)=== false)
+    {	
+     var msg="Please no special characters";
+     document.getElementById("description_err").innerHTML="";     
     }   	
-	
+	else 
+        {
+           hasErrors = false;
+           description.classList.remove('good');
+        }
 
+        
+        
 	//this is saving to local storage		
-var name = document.getElementById('fullname').value;
-var email = document.getElementById('email').value;
-var phone = document.getElementById('phone').value;
-var description = document.getElementById('description').value;
+
+if ( true   )
+{ 
+    saveData();
+}
+ 
+ 
+ 
+ 
+ 
+ }
+   //Event listener that will activate the save data button once it is clicked 
+submitBtn.addEventListener('click', submitForm);
+
 
 var data = [];
              
-                         
-data.push({ 
- "fullname": [name]
-});
- data.push({ 
- "Email": [email]  
-});
- data.push({ 
- "Phone": [phone]  
-});
-data.push({ 
-"description": [description]   
-});
+    function saveData(){
+        
+        var currentData = {
+              "FullName" : "",
+              "Email" : "",
+              "Phone" : "",
+              "Description" : ""
+            };
+     
+    
+    currentData.FullName = fullname.value;
+    currentData.Email = email.value;
+    currentData.Phone = phone.value;
+    currentData.Description = description.value;
+
+
+data.push(currentData);
+
+   
+            
 localStorage.setItem('data', JSON.stringify(data) );
             
             var savedData = localStorage.getItem('data');
             
             console.log(savedData);
-			
-			
+            console.log(JSON.parse(savedData));
 		
-			
-			
-			
-			/////
-			//document.getElementById("tableData").innerHTML = localStorage.inputs[0].value;
-			//document.getElementById("tableData").innerHTML = localStorage.email;
-			//var inputs = document.getElementsByClassName("CSSTableGenerator"); 
-           // Name = inputs[0].value; 
-           // E-mail = inputs[1].value; 
-           // Phone = inputs[2].value; 
-           // Description = inputs[3].value; 
-
-		   
-		   
-		   
-		   
-
-
- 
- }  
-   //Event listener that will activate the save data button once it is clicked 
-submitBtn.addEventListener('click', submitForm);
+ } 
 
 	
 	//this makes the Clear all data button clear all data on local storage
-var clearBtn = document.getElementById('clearData');
+//var clearBtn = document.getElementById('clearData');
  
  
- function clear () {
+ function clear () 
+ {
  localStorage.clear();
  }
+ var clearBtn = document.getElementById('clearData');
 clearBtn.addEventListener('click', clear);
